739b69d78c6286150bec8053d9ad13bec9dd995a
Danish Malik <cyanide9102@gmail.com>
2022-10-26 17:42:17 +0500

    Created products component

--- src/BlazorDevApp.Web/Program.cs
+++ src/BlazorDevApp.Web/Program.cs
@@ -1,19 +1,23 @@
-using BlazorDevApp.Web.Data;
+using BlazorDevApp.Core.Interfaces;
+using BlazorDevApp.Infrastructure.Data;
+
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection")));
+
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
+
+builder.Services.AddTransient(typeof(IRepository<>), typeof(EfRepository<>));
+builder.Services.AddTransient(typeof(IReadRepository<>), typeof(EfRepository<>));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
