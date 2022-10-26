c74e9dec9aa7acca2b891474c56212c4d6c6da40
Danish Malik <cyanide9102@gmail.com>
2022-10-26 17:49:52 +0500

    Created add product razor component

---
+++ src/BlazorDevApp.Web/Models/AddProductModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+#nullable disable
+
+namespace BlazorDevApp.Web.Models;
+public class AddProductModel
+{
+    [Required, StringLength(256)]
+    public string Name { get; set; }
+    [Required, StringLength(1024)]
+    public string Description { get; set; }
+    [Required]
+    public decimal Price { get; set; }
+}
